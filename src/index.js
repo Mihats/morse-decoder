311ec02d757f978da5756ae17253b6890eae6e95
Mihats <misha.ts2104@gmail.com>
2020-10-19 21:47:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,44 +1,73 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let result = "";
+
+    let exprArray = expr.split("*");
+    exprArray = exprArray.filter(entry => entry.trim() != '');
+
+    exprArray.forEach(element => {
+
+
+        let subArray = element.match(/.{1,10}/g);
+
+        subArray.forEach(str => {
+            let morseString = "";
+            let subString = str.match(/.{1,2}/g);
+
+            subString.forEach(subSubString => {
+
+                if (subSubString === "10") morseString += ".";
+                if (subSubString === "11") morseString += "-";
+            })
+
+
+            result += MORSE_TABLE[morseString];
+
+        })
+        result += " ";
+
+    });
+
+    return result.slice(0, result.length - 1);
 }
 
 module.exports = {
